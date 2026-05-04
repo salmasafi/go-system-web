a3849dabcf4fac9b45ef71d4aea0f14b31b84590
Salma <salmasafi126@gmail.com>
2026-05-05 02:44:06 +0300

    Fix: Build without service worker + cache busting v3

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -36,8 +36,4 @@ if (!window._flutter) {
 }
 _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c34c","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
-_flutter.loader.load({
-  serviceWorkerSettings: {
-    serviceWorkerVersion: "2483515684"
-  }
-});
+_flutter.loader.load();
